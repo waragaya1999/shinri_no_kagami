d265151bbb36028592511e72f9669d002384db3e
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-04 15:10:08 +0900

    [fix]ディレクトリ構造の修正

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.css"
+import { AppProps } from "next/app"
+
+const App = ({ Component, pageProps }: AppProps) => (
+    <>
+        <Component {...pageProps} />
+    </>
+)
+
+export default App
