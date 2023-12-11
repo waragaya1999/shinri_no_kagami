3d703ca6125848bdb4deabd130fc085954193b98
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-11 11:27:59 +0900

    [add]ヘッダーとフッター追加

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <footer className={"fixed bottom-0 w-full h-32 bg-gray-300"}></footer>
+    )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <header className="w-full h-16 flex justify-center items-center bg-gray-300 text-4xl text-black z-10"></header>
+    )
+}
