233f66af0858b4d60f45fc18bc454411a03c3055
MeitoKuriyama <meito.kuri@gmail.com>
2023-12-18 15:55:13 +0900

    [test] プルリクテスト

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     return (
         <header className="w-full h-16 flex justify-center items-center bg-gray-300 text-4xl text-black z-10">
             <button onClick={() => handleOnClick()} className=" z-10">
-                test
+                testaaa
             </button>
             {capturedPhoto && <img src={capturedPhoto} alt="" />}
         </header>
