42e1556a626978132da9a849a64ab3d05e87a955
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-24 16:59:12 +0900

    [add]最小単位のコンポーネントディレクトリの追加

--- components/ExpressionsGraph.tsx
+++ components/ExpressionsGraph.tsx
@@ -1,30 +1,11 @@
 import { ExpressionsDto } from "@/types/ExpressionsDto"
-import React from "react"
+import ExpressionBar from "@/components/atoms/ExpressionBar"
+import ExpressionMark from "@/components/atoms/ExpressionMark"
 
 type Props = {
     expressions: ExpressionsDto
 }
 export default function ExpressionsGraph({ expressions }: Props) {
-    const expressionBar = (expression: number) => {
-        return (
-            <div className={"flex justify-center items-end w-1/6 h-[90%]"}>
-                <div
-                    className={"w-1/3 bg-gray-400 rounded-t"}
-                    style={{
-                        height: `${expression * 100}%`,
-                    }}
-                ></div>
-            </div>
-        )
-    }
-    const expressionMark = (emoji: string) => {
-        return (
-            <li className={"flex w-1/6 items-center justify-center text-2xl"}>
-                {emoji}
-            </li>
-        )
-    }
-
     return (
         <div
             className={
@@ -36,21 +17,21 @@ export default function ExpressionsGraph({ expressions }: Props) {
                     "flex justify-between items-end w-full h-[70%] bg-gray-300 rounded-t-2xl"
                 }
             >
-                {expressionBar(expressions.neutral)}
-                {expressionBar(expressions.happy)}
-                {expressionBar(expressions.sad)}
-                {expressionBar(expressions.angry)}
-                {expressionBar(expressions.fearful)}
-                {expressionBar(expressions.surprised)}
+                {ExpressionBar(expressions.neutral)}
+                {ExpressionBar(expressions.happy)}
+                {ExpressionBar(expressions.sad)}
+                {ExpressionBar(expressions.angry)}
+                {ExpressionBar(expressions.fearful)}
+                {ExpressionBar(expressions.surprised)}
             </div>
             <div className={"w-full h-[30%]"}>
                 <ul className={"flex w-full h-full"}>
-                    {expressionMark("😐")}
-                    {expressionMark("😆")}
-                    {expressionMark("😭")}
-                    {expressionMark("😠")}
-                    {expressionMark("🤪")}
-                    {expressionMark("😲")}
+                    {ExpressionMark("😐")}
+                    {ExpressionMark("😆")}
+                    {ExpressionMark("😭")}
+                    {ExpressionMark("😠")}
+                    {ExpressionMark("🤪")}
+                    {ExpressionMark("😲")}
                 </ul>
             </div>
         </div>
---
+++ components/atoms/ExpressionMark.tsx
@@ -0,0 +1,7 @@
+export default function ExpressionMark(emoji: string) {
+    return (
+        <li className={"flex w-1/6 items-center justify-center text-2xl"}>
+            {emoji}
+        </li>
+    )
+}
