aea630103376711e67133bc71256b3648371b668
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-26 22:15:56 +0900

    [update]OtenkiInfo完成

--- components/OtenkiInfo.tsx
+++ components/OtenkiInfo.tsx
@@ -1,5 +1,5 @@
-import React from "react"
 import { WeatherInfoDto } from "@/types/WeatherInfoDto"
+import WeatherIcon from "@/components/atoms/WeatherIcon"
 
 type Props = {
     weather: WeatherInfoDto | undefined
@@ -8,17 +8,38 @@ type Props = {
 
 export default function OtenkiInfo({ weather, prefecture }: Props) {
     return (
-        <div className={"fixed z-100 top-[5rem] left-[3rem]"}>
-            {weather?.weather[0].main}
-            <br />
-            {prefecture}
-            <br />
-            {weather?.main.temp &&
-                `${(weather?.main.temp - 273.15).toFixed(1)}℃`}
-            <br />
-            {weather?.main.humidity}%
-            <br />
-            {weather?.main.pressure}hPa
+        <div
+            className={
+                "absolute flex justify-between w-fit top-[2%] left-[6%] rounded-xl bg-black bg-opacity-30 z-100 p-3"
+            }
+        >
+            <div className={"w-10 mr-2"}>
+                {weather?.weather[0].icon && (
+                    <img
+                        src={`/images/${weather?.weather[0].icon}.svg`}
+                        alt={""}
+                    />
+                )}
+            </div>
+            <div className={"w-fit text-gray-200"}>
+                <div className={"flex items-center"}>
+                    {WeatherIcon("location")}
+                    <p>{prefecture}</p>
+                </div>
+                <div className={"flex items-center"}>
+                    {WeatherIcon("thermometer")}
+                    <p className={"mr-1"}>
+                        {weather?.main.temp &&
+                            `${(weather?.main.temp - 273.15).toFixed(1)}℃`}
+                    </p>
+                    {WeatherIcon("humidity")}
+                    <p>{weather?.main.humidity}%</p>
+                </div>
+                <div className={"flex items-center"}>
+                    {WeatherIcon("barometer")}
+                    <p>{weather?.main.pressure}hPa</p>
+                </div>
+            </div>
         </div>
     )
 }
