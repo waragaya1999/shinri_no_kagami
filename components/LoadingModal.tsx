2d48ecf8cfed4ac721022cde76e71b34e89ad72d
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-26 23:08:49 +0900

    [update]Loadingのドット追加

--- components/LoadingModal.tsx
+++ components/LoadingModal.tsx
@@ -1,23 +1,30 @@
-import { MutableRefObject } from "react"
 import { WeatherInfoDto } from "@/types/WeatherInfoDto"
+import { useState } from "react"
+import * as timers from "timers"
 
 type Props = {
-    videoRef: MutableRefObject<HTMLVideoElement | null>
     weather: WeatherInfoDto | undefined
 }
-export default function LoadingModal({ videoRef, weather }: Props) {
+export default function LoadingModal({ weather }: Props) {
+    const [timer, setTimer] = useState(0)
+    setInterval(() => {
+        const newTimer = timer + 1
+        setTimer(newTimer)
+    }, 1000)
     return (
         <>
-            {!videoRef ||
-                (!weather && (
-                    <div
-                        className={
-                            "flex fixed justify-center items-center w-screen h-screen bg-white z-[999]"
-                        }
-                    >
-                        <h1>Loading</h1>
-                    </div>
-                ))}
+            {!weather && (
+                <div
+                    className={
+                        "flex fixed justify-center items-center w-screen h-screen bg-white z-[999]"
+                    }
+                >
+                    <h1>
+                        Loading
+                        {timer % 3 === 0 ? "." : timer % 3 === 1 ? ".." : "..."}
+                    </h1>
+                </div>
+            )}
         </>
     )
 }
