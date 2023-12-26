8a014d6e9dfad0f8660145d5cd48aad3a2141f39
waragaya1999 <mochimochidaifucu@icloud.com>
2023-12-26 23:08:01 +0900

    [fix]リファクタリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,22 @@
-import { useEffect, useLayoutEffect } from "react"
+import { useEffect } from "react"
 import { useOtenkiApi } from "@/hooks/useOtenkiApi"
-import { useVideo } from "@/hooks/useVideo"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import ExpressionsGraph from "@/components/ExpressionsGraph"
-import OtenkiInfo from "@/components/OtenkiInfo"
-import "tailwindcss/tailwind.css"
-import Video from "@/components/Video"
 import LoadingModal from "@/components/LoadingModal"
+import Video from "@/components/Video"
 
 export default function Home() {
-    const { getOtenkiApi, muniCd, prefecture, latlon, weather } = useOtenkiApi()
-    const { detectFace, expressions, videoRef } = useVideo()
+    const { getOtenkiApi, prefecture, weather } = useOtenkiApi()
 
     useEffect(() => {
         getOtenkiApi()
-        // detectFace()
     }, [])
 
     return (
         <>
-            <LoadingModal videoRef={videoRef} weather={weather} />
+            <LoadingModal weather={weather} />
             <Header />
-            <div className={"relative z-10"}>
-                {/* <video
-                    id="video"
-                    ref={videoRef}
-                    autoPlay
-                    playsInline
-                    muted
-                    className={"w-[94%] rounded-3xl m-auto"}
-                /> */}
-
-                <Video />
-
-                <ExpressionsGraph expressions={expressions} />
-                <OtenkiInfo weather={weather} prefecture={prefecture} />
-            </div>
+            <Video weather={weather} prefecture={prefecture} />
             <Footer />
         </>
     )
