c2f7bee7de234a3e089d3c6c397978b077df21bd
MeitoKuriyama <meito.kuri@gmail.com>
2023-12-28 21:23:04 +0900

    [nothing] ブランチはやしたいので

--- pages/otenki.tsx
+++ pages/otenki.tsx
@@ -9,9 +9,9 @@ export default function otenki() {
         getOtenkiApi()
     }
 
-    useEffect(() => {
-        getOtenkiApi()
-    })
+    // useEffect(() => {
+    //     getOtenkiApi()
+    // })
 
     return (
         <>
