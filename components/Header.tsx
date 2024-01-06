e54fc85c26cc0d3c329bfe3c7ad35dda4e944837
waragaya1999 <mochimochidaifucu@icloud.com>
2024-01-06 20:58:49 +0900

    [fix] スタイル調整

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="fixed flex items-center w-full h-16 border-b">
+        <header className="fixed flex items-center w-full h-16 border-b bg-white z-10">
             <h1 className={"text-xl p-4"}>shinri_no_kagami</h1>
         </header>
     )
